bbe0e46d1195ce987f2798d6cc61c89e785e0ea7
aa <yeongjae.jeong@gmail.com>
2025-03-04 19:42:49 +0900

    dfdd

---
+++ c_example/ddd.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 1;
+    int b = 2;
+    
+    printf("%d\n", a);
+
+    return 0;
+}
---
+++ c_example/test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+
+    char* my;
+
+    scanf("%s", my);
+    
+    return 0;
+}
