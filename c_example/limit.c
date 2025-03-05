08e77d5e08bd3b2cdd73cc86334f7d180f20d590
aa <yeongjae.jeong@gmail.com>
2025-03-05 12:05:21 +0900

    INT_MAX

--- c_example/limit.c
+++ c_example/limit.c
@@ -6,5 +6,10 @@ int main()
     printf("hello, world\n");
     printf("char_bit : %d\n", CHAR_BIT);
     printf("char_bit : %d\n", CHAR_MAX);
+
+    printf("INT_MAX : %d \t INT_MIN : %d\n", INT_MAX, INT_MIN);
+    printf("LONG_MAX : %ld \t LONG_MIN : %ld\n", LONG_MAX, LONG_MIN);
+
+
     return 0;
 }
