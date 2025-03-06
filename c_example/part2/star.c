8a5841a27a4df21500d6926e22ac19fa879c86a8
aa <yeongjae.jeong@gmail.com>
2025-03-07 08:32:03 +0900

    adf

--- c_example/part2/star.c
+++ c_example/part2/star.c
@@ -18,22 +18,15 @@
 // }
 
 // //2번
-int main (){
-    for(int i = 1; i <= 5 ; ++i ){
-        for(int j = 1; j <= i; ++j ){
+int main(void){
+    for (int i = 1; i <= 5; ++i){
+        for (int j = 1; j <= 6 - i; ++j){
+            printf(" ");
+        }
+        for (int j = 1; j <= 2 * i - 1; ++j){
             printf("*");
         }
         printf("\n");
-    }
-    for(int i = 1; i<= 5; ++i){
-        for(int j = 1; j <= 6 -i; ++j ){
-            printf(" ");
-        }
-    
-        for(int j = 1; j <= 2*i-1; ++j ){
-            printf("*");
-         }
-         printf("\n");
     }
     return 0;
 }
