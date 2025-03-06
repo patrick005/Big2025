962aa79c4140186b5eb3c15b0b01e643eecd751a
aa <yeongjae.jeong@gmail.com>
2025-03-06 14:10:54 +0900

    ADS

--- c_example/part1/ddd.c
+++ c_example/part1/ddd.c
@@ -2,11 +2,13 @@
 
 int main()
 {
-    int a = 1;
-    int b = 0x10;
+    // int a = 1;
+    // int b = 0x10;
     int c = 010;
     
     printf("%d\n", c);
+
+    // int c = 2;
 
     return 0;
 }
