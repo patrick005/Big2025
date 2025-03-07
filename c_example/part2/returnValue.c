9636016227ed4d017809432997ddca175f40992d
aa <yeongjae.jeong@gmail.com>
2025-03-07 12:23:15 +0900

    asdfgh

--- c_example/part2/returnValue.c
+++ c_example/part2/returnValue.c
@@ -5,16 +5,16 @@
 
 
 int f(void);
-int f2(int *pResult):
+int f2(int *pResult);
 
 
 int main(void){
     int f1_result = f();
-    printf("f1의 반환 결과는 : %d", f1_result);
+    printf("f1의 반환 결과는 : %d\n", f1_result);
 
     int f2_result;
     f2(&f2_result);
-    printf("f2의 처리 결과는 : %d", f2_result);
+    printf("f2의 처리 결과는 : %d\n", f2_result);
 
     return 0;
 }
@@ -25,6 +25,4 @@ int f(void){
 
 int f2(int *pResult){
     *pResult = 100;
-    return; 
 }
-
