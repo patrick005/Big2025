96391b772a1c7de27d3e103a0c6a66b8fff1f425
aa <yeongjae.jeong@gmail.com>
2025-03-10 15:26:27 +0900

    1235

---
+++ c_example/part3/arrayAssignment.c
@@ -0,0 +1,26 @@
+//arrayAssignment.c
+
+#include <stdio.h>
+ 
+int main(void){
+    int nums1[] = {1, 2, 3, 4, 5};
+    int nums2[5];
+
+    //nums2 = nums1; // 이처럼 쓰면 안된다는 것을 보여주기 위함
+    //상수가 앞에 나올순 없음, 함수명도 상수 취급임
+
+    //deep copy 하는 방법, 다른메모리에 다 복사한다
+    for(int i = 0; i < 5; ++i){
+        nums2[i] = nums1[i];     
+    }
+    printf("nums1 의 주소값 %p\n", nums1);
+    printf("nums2 의 주소값 %p\n", nums2);
+    // 서로 독립적인 메모리이다.
+    nums2[3] = 13;
+    for(int i = 0; i < 5; ++i){
+        printf("nums1 %d : %d, \n", i , nums1[i]);
+        printf("nums2 %d : %d, \n", i , nums2[i]);
+    }
+    
+    return 0;
+}
