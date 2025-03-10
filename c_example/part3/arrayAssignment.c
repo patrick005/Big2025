5264da32b729c3cd4f886cecd00129aae495e3b3
aa <yeongjae.jeong@gmail.com>
2025-03-10 19:30:24 +0900

    1234

--- c_example/part3/arrayAssignment.c
+++ c_example/part3/arrayAssignment.c
@@ -1,35 +1,37 @@
-//arrayAssignment.c
+// arrayAssignment.c
 
 #include <stdio.h>
- 
-int main(void){
+
+int main(void)
+{
     int nums1[] = {1, 2, 3, 4, 5};
     int nums2[5];
 
-    //nums2 = nums1; // 이처럼 쓰면 안된다는 것을 보여주기 위함
-    //상수가 앞에 나올순 없음, 배열의 함수명은 1번째 값의 자리를 상수로 취급
+    // nums2 = nums1; // 이처럼 쓰면 안된다는 것을 보여주기 위함
+    // 상수가 앞에 나올순 없음, 배열의 함수명은 1번째 값의 자리를 상수로 취급
 
-    //deep copy 하는 방법, 다른메모리에 다 복사한다
-    for(int i = 0; i < 5; ++i){
-        nums2[i] = nums1[i];     
+    // deep copy 하는 방법, 다른메모리에 다 복사한다
+    for (int i = 0; i < 5; ++i)
+    {
+        nums2[i] = nums1[i];
     }
     printf("nums1 의 주소값 %p\n", nums1);
     printf("nums2 의 주소값 %p\n", nums2);
     // 서로 독립적인 메모리이다.
     nums2[3] = 14;
-    //shallow copy, 메모리를 공유하는 방법
+    // shallow copy, 메모리를 공유하는 방법 - 카피라기보단 싱크느낌
     int *nums3;
     nums3 = nums1;
     printf("nums3 의 주소값 %p\n", nums3);
     nums3[2] = 23;
 
-    for(int i = 0; i < 5; ++i){
+    for (int i = 0; i < 5; ++i)
+    {
         printf("----------------\n");
-        printf("nums1 %d : %d, \n", i , nums1[i]);
-        printf("nums2 %d : %d, \n", i , nums2[i]);
-        printf("nums3 %d : %d, \n", i , nums3[i]);
+        printf("nums1 %d : %d, \n", i, nums1[i]);
+        printf("nums2 %d : %d, \n", i, nums2[i]);
+        printf("nums3 %d : %d, \n", i, nums3[i]);
     }
-    
-    
+
     return 0;
 }
