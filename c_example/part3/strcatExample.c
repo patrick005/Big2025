0f882fc924b5f4313cc8d31a62118912b55350d2
aa <yeongjae.jeong@gmail.com>
2025-03-11 16:01:08 +0900

    123

--- c_example/part3/strcatExample.c
+++ c_example/part3/strcatExample.c
@@ -11,7 +11,7 @@ int main(void){
     int str2len = strlen(str2);
     printf("str1Len : %d str2Len : %d \n", str1len, str2len);
 
-    char str3[20];
+    char str3[30];
     strcpy(str3, str1);
     str3[0] = 'H';
     printf("%s\n", str3);
@@ -21,7 +21,9 @@ int main(void){
     }else{
         printf("두 문자열은 %d 개만큼 다르다.\n", strcmp(str1, str3));
     }
-
+    
+    strcat(str3, str2);
+    printf("%s\n", str3);
 
     return 0;
 }
---
+++ c_example/part3/string/string.c
@@ -0,0 +1,28 @@
+#include "string.h"
+
+int my_strlen(const char *s){
+    int sum;
+    for(int i = 0; i < 10; ++i){
+     //sumArrayFunction.c
+
+     
+    }
+}
+
+
+void my_strcpy(char *des, const char *src){
+
+//strcatExample.c
+
+}
+
+int my_strcmp(const char *s1, const char *s2){
+
+
+}
+
+void my_strcat(char *des, const char *src){
+
+
+
+}
