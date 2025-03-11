2cfb414b8d8222ec09dad9f4fe723417c6c30eec
aa <yeongjae.jeong@gmail.com>
2025-03-11 12:12:07 +0900

    4523

---
+++ c_example/part3/date/main.c
@@ -0,0 +1,24 @@
+#include "date.h"
+
+int main(void){
+    // Date d;
+    // d.year = 2025;
+    // d.month = 3;
+    // d.day = 11;
+
+    // Date d = {2024, 3, 11};
+
+    Date d = {
+        .year = 2025,
+        .month = 3,
+        .day = 11,
+        .temp = {1, 2, 3, 4, 5, 6}};
+
+    Date today;
+    today = d;
+    printDate(&today);
+    today.temp[0] = 10; // 배열이 복사 됨.
+    printDate(&d);
+
+    return 0;
+}
