8ea476fa0c84902c8d7710f2797356d2cbc2d5f0
aa <yeongjae.jeong@gmail.com>
2025-03-11 11:11:22 +0900

    2345

--- c_example/part3/myRandom/myRandom.c
+++ c_example/part3/myRandom/myRandom.c
@@ -1,31 +1,12 @@
 // myRandom.c
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+static int seed = 1;
+// extern int seed;
 
-void my_srand(int s);
-int my_rand(void);
-
-int main(void)
-{
-    my_srand(time(NULL));
-    for (int i = 0; i < 10; ++i)
-    {
-        int num = my_rand();
-        printf("%d ,", num);
-    }
-    printf("\n");
-    return 0;
-}
-
-int seed = 1;
-void my_srand(int s)
-{
+void my_srand(int s){
     seed = s;
 }
 
-int my_rand(void)
-{
+int my_rand(void){
     seed = seed * 11035152245 + 12345;
     return ((unsigned)(seed / 65535)) % 32768;
 }
---
+++ c_example/part3/myRandom/myRandom.h
@@ -0,0 +1,8 @@
+#pragma once // 한번만 한다는 확신이 있을 경우, 사용
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void my_srand(int s);
+int my_rand(void);
