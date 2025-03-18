af1ddd55f0fc13eb6ede7c88ea5ff9a7a730088e
aa <yeongjae.jeong@gmail.com>
2025-03-18 16:09:14 +0900

    shft

---
+++ atmega128/ch1_1_led.c
@@ -0,0 +1,16 @@
+//led toggle
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int main(void){
+
+    while(1){
+        PORTC = 0x00;       //0b00000000
+        _delay_ms(1000);    //1초대기
+        PORTC = 0x0F;       //0b00001111
+        _delay_ms(1000);    //1초대기
+    }
+
+    return 0;
+}
