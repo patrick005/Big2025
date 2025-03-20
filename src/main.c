5d5bb8f8a975518e0d168e9f1694b706ac391cd7
aa <yeongjae.jeong@gmail.com>
2025-03-20 10:15:32 +0900

    634

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@ int main(){
     uart0Init();
     unsigned char rxData;
     uint8_t numbers[] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x27, 0x7F, 0x67};
+    DDRA = 0xff;
 
     while (1){
         rxData = uart0Receive();
