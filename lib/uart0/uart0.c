42aaf42330ec1e106d18065091499ef4787cc2aa
aa <yeongjae.jeong@gmail.com>
2025-03-20 12:08:40 +0900

    12335

--- lib/uart0/uart0.c
+++ lib/uart0/uart0.c
@@ -2,22 +2,27 @@
 
 void uart0Init(void)
 {
-    UCSR0A |= 0x00; // 2배속 모드
-    UCSR0B = 0x18;       // 0b00011000 Rx, Tx enable
-    UCSR0C = 0x06;      // 0b00010110  비동기, no Parity, 1 stop bit
+    // UCSR0A |= _BV(U2X1); // 2배속 모드
+    // UCSR0B = 0x18;       // 0b00011000 Rx, Tx enable
+    // UCSR0C |= 0x06;      // 0b00010110  비동기, no Parity, 1 stop bit
+
+    // UBRR0H = 0x00;
+    // UBRR0L = 207; //  9600 bps
+    UCSR0A = 0x00;
+    UCSR0B = 0x18; // 0b00011000 Rx, Tx enable
+    UCSR0C = 0x16; // 0b00010110  비동기, no Parity, 1 stop bit
 
     UBRR0H = 0x00;
-    // UBRR0L = 207; // 115200 bps --> 9600 bsp  
-    UBRR0L = 0x07;
+    UBRR0L = 0x07; // 115200 bps
 }
 
 void uart0Transmit(char data)
 {
-    while (!(UCSR0A & _BV(UDRE0))){
+    while (!(UCSR0A & _BV(UDRE0)))
         ;
-        UDR0 = data;
-    }
+    UDR0 = data;
 }
+
 unsigned char uart0Receive(void)
 {
     while (!(UCSR0A & _BV(RXC0))) // 문자 버퍼에 있으면 루프 탈출
