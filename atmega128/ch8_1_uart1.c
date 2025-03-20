35d45ddfbb1e1186392a6cfcd66baefbd6a28e5f
aa <yeongjae.jeong@gmail.com>
2025-03-20 12:08:40 +0900

    12335

--- atmega128/ch8_1_uart1.c
+++ atmega128/ch8_1_uart1.c
@@ -1,11 +1,12 @@
 #include <avr/interrupt.h>
 #include <avr/io.h>
-#include <uart0.h>
+#include "uart0.h"
 
 int main(){   
     uart0Init();
     unsigned char rxData;
     uint8_t numbers[] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x27, 0x7F, 0x67};
+    DDRA = 0xff;
 
     while (1){
         rxData = uart0Receive();
--- lib/uart0/uart0.h
+++ lib/uart0/uart0.h
@@ -1,10 +1,17 @@
 #ifndef UART0_H_
 #define UART0_H_
+#include <stdio.h>
+
+
 
 void uart0Init(void);
 void uart0Transmit(char data);
 unsigned char uart0Receive(void);
 void uart0PrintString(char *str);
 void uart0Print1ByteNumber(unsigned char n);
+
+
+FILE OUTPUT = FDEV_SETUP_STREAM(uart0Transmit, NULL, _FDEV_SETUP_WRITE);    //stdio사용설정
+FILE INPUT = FDEV_SETUP_STREAM(NULL, uart0Receive, _FDEV_SETUP_READ);       //stdio사용설정
 
 #endif // UART0_h_
