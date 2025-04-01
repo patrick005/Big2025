4a35eaa37103b3de86cf8c9515672d673a30c0ab
patrick <yeongjae.jeong@gmail.com>
2025-04-01 17:47:08 +0900

    24357

---
+++ raspberryPi/module/.modules.order.cmd
@@ -0,0 +1,1 @@
+savedcmd_/home/patrick/Big2025/raspberryPi/module/modules.order := {   echo /home/patrick/Big2025/raspberryPi/module/module_exam.o; :; } > /home/patrick/Big2025/raspberryPi/module/modules.order
