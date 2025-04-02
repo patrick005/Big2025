e4bb80b7ef2edee6bfa9e9eb7e2d68cc0f089acb
patrick <yeongjae.jeong@gmail.com>
2025-04-02 17:28:45 +0900

    1578

--- raspberryPi/i2c_servo.c
+++ raspberryPi/i2c_servo.c
@@ -15,29 +15,31 @@ int main(void){
     i2C_init(rgb_servo_fd, 0x79); // 주파수 50Hz 설정 20ms 
     // 프리스케일값 = (오실러레이터 주파수 / (4096 * 원하는 주파수)) - 1
     // 2500000 / ( 4096 * ( 0x79 + 1)) = 50.028817
-    int k = 0;
-    int servo_0 = 1;
-    int servo_180 = 1000;
-    // int servo_0 = 800;
-    // int servo_180 = 4600;
-for(k=0; k<10; k++){
+
+    // int servo_0 = 120;
+    // int servo_180 = 500;
+    int servo_0 = 100; //90도는 380정도이다;; 300이 아니었다...
+    int servo_180 = 500;
+// for(int k=0; k<3; k++){
     wiringPiI2CWriteReg16(rgb_servo_fd, PCA9685_LED_ON_L + 12, 0); //servo1 on_off
     wiringPiI2CWriteReg16(rgb_servo_fd, PCA9685_LED_ON_L + 14, servo_0); //servo1 pwm duty
-    
+    delay(10);
     wiringPiI2CWriteReg16(rgb_servo_fd, PCA9685_LED_ON_L + 16, 0); //servo2 on_off
     wiringPiI2CWriteReg16(rgb_servo_fd, PCA9685_LED_ON_L + 18, servo_0); //servo2 pwm duty
-    delay(1000);
+    delay(800);
 
     wiringPiI2CWriteReg16(rgb_servo_fd, PCA9685_LED_ON_L + 14, servo_180/2); //servo1 pwm duty
     wiringPiI2CWriteReg16(rgb_servo_fd, PCA9685_LED_ON_L + 18, servo_180/2); //servo2 pwm duty
-    delay(1000);
+    delay(800);
 
     wiringPiI2CWriteReg16(rgb_servo_fd, PCA9685_LED_ON_L + 14, servo_180); //servo1 pwm duty
     wiringPiI2CWriteReg16(rgb_servo_fd, PCA9685_LED_ON_L + 18, servo_180); //servo2 pwm duty
-    delay(1000);
-}
+    delay(800);
+// }
     //free: 전압이 계속 흘러서 무리가 갈수 있으니 해제도 해주기
+    wiringPiI2CWriteReg16(rgb_servo_fd, PCA9685_LED_ON_L + 12, 0); //servo1 on_off
     wiringPiI2CWriteReg16(rgb_servo_fd, PCA9685_LED_ON_L + 14, 0); //servo1 on_off
+    wiringPiI2CWriteReg16(rgb_servo_fd, PCA9685_LED_ON_L + 16, 0); //servo2 on_off
     wiringPiI2CWriteReg16(rgb_servo_fd, PCA9685_LED_ON_L + 18, 0); //servo2 on_off
     
     return 0;
@@ -46,7 +48,7 @@ for(k=0; k<10; k++){
 
 void i2C_init(int fd, int freq){
     
-    wiringPiI2CWriteReg8(fd, PCA9685_MODE1, 0x01);    // 리셋
+    wiringPiI2CWriteReg8(fd, PCA9685_MODE1, 0x10);    // 리셋
     delay(10);                                        // 리셋 후 대기
     wiringPiI2CWriteReg8(fd, PCA9685_PRESCALE, freq); // 주파수 60Hz 설정
     wiringPiI2CWriteReg8(fd, PCA9685_MODE1, 0xA1);
