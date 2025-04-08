10c9b4d9e3236bd28bb99e22ae5055dcf70de27c
aa <yeongjae.jeong@gmail.com>
2025-04-08 18:54:41 +0900

    stack4

---
+++ ds/stack5/stack.c
@@ -0,0 +1,37 @@
+#include "stack.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+void initStack(Stack *ps){
+    ps->tos = 0;
+}
+
+void push(Stack *ps, int data){
+    // s.array[s.tos] = data;
+    // ++s.tos;
+
+    //full 예외처리
+    if(ps->tos >= STACKSIZE ){
+        fprintf(stderr, "stack is full.\n");
+        exit(1);
+    }
+    (*ps).array[(*ps).tos] = data;
+    ++(*ps).tos;
+    //ps->array[ps->tos] = data;
+    //++ps->tos;
+}
+
+int pop(Stack *ps){
+    // --s.tos;
+    // return s.array[s.tos];
+
+    //empty 예외처리
+    if (ps->tos <= 0){
+        fprintf(stderr, "stack is empty.\n");
+        exit(2);
+    }
+    
+    --(*ps).tos;
+    return (*ps).array[(*ps).tos];
+}
+
