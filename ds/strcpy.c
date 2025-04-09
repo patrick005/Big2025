46981c152b4f63ca174a5df4d4d75fa5b5d803c6
aa <yeongjae.jeong@gmail.com>
2025-04-09 16:10:37 +0900

    1234

---
+++ ds/strcpy.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void my_strcpy(char *des, const char *src){
+    /*int i;
+    for(i = 0; src[i]!=0; ++i){
+        des[i] = src[i]; 
+     
+    }
+    des[i] = '\0';*/
+    while (*des++ = *src++){
+        ;
+    }
+}
+
+
+ 
+int main(void){
+    char str[100];
+    char *str2 = "hello, world";
+
+    my_strcpy(str, str2);
+    printf("str1: %s\n", str);
+
+    return 0;
+}
