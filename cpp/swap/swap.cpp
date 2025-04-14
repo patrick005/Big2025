47df71a654e6c4ab68333abc11bd21721b060962
aa <yeongjae.jeong@gmail.com>
2025-04-14 15:35:47 +0900

    coment swap.cpp

--- cpp/swap/swap.cpp
+++ cpp/swap/swap.cpp
@@ -15,6 +15,9 @@ void swap3(int &ra, int &rb){ //ver. reference
     ra = rb;
     rb = tmp;
 }
+//reference를 활용시엔 함수 overloading을 사용한 중첩은 하지마라. 꼬일 수 있으니 이름을 다르게 줘라
+// 사유: 1,2번의 경우 포인터 초기화를 하는 NULL을 0으로 대체가 가능하기에 NULL인지, 정수형 0인지에 대한 오류
+// 2,3번의 경우 둘다 포인터이기에 어떤게 우선순위로 들어가는지, 둘다 적용되는지에 대한 문제점
 
 int main(){
     int a = 100;
