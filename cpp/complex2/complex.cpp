7cf1972a99866d3d9be8576a5f8504e00a282d31
aa <yeongjae.jeong@gmail.com>
2025-04-14 16:58:03 +0900

    reference를 적용한 complex2파일 작성

--- cpp/complex2/complex.cpp
+++ cpp/complex2/complex.cpp
@@ -18,6 +18,35 @@ Complex::~Complex(){
 
 }
 
+// void Complex::operator=(Complex c){
+//     this->re = c.re;
+//     this->im = c.im;
+// }
+
+// void Complex::operator=(const Complex *pc){
+//     this->re = pc->re;
+//     this->im = pc->im;
+// }
+void Complex::operator=(const Complex& rc){
+    this->re = rc.re;
+    this->im = rc.im;
+}
+// int Complex::operator==(const Complex& rc){
+//     if((this->re = rc.re )&& (this->im = rc.im)){
+//         return 1;
+//     }else{
+//         return 0;
+//     }
+// }
+bool Complex::operator==(const Complex& rc){
+    // if((this->re = rc.re )&& (this->im = rc.im)){
+    //     return true;
+    // }else{
+    //     return false;
+    // }
+    return (this->re == rc.re && this->im == rc.im);
+}
+
 double Complex::real(){
 
     return this->re;
