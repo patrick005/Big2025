77b7d4d22f81a7a43ec73a179e19f6fc842a594d
aa <yeongjae.jeong@gmail.com>
2025-04-15 12:26:15 +0900

    rational2에 대입연산자와 비교연산자 추가

--- cpp/rational2/main.cpp
+++ cpp/rational2/main.cpp
@@ -5,10 +5,18 @@
 int main(){
     Rational r1(3, 4);  //3/4
     Rational r2(3);     //3/1
-    Rational r3;        //0/1
+    Rational r3(6, 9);        //0/1 ==>비교분석을 위해 (0,0)에서 (6,8)로 변경, (6,9)로도 확인 환료
 
-    r3.SetNum(r1.getNum());
-    r3.SetDenom(r1.getDenom());
+    // r3.SetNum(r1.getNum());
+    // r3.SetDenom(r1.getDenom());
+
+    // r3 = r1; //==> operator= 확인 완료, operator==확인을 위한 주석처리
+
+    if(r1 == r3){// operator==(c1, c3) or c1.operator==(c3)
+        std::cout << "r1 and r3 are equal" << std::endl;
+    }else{
+        std::cout << "r1 and r3 are noet equal" << std::endl;
+    }
 
     std::cout << "r1 : " << r1.getNum() << "/" << r1.getDenom() << std::endl;
     std::cout << "r2 : " << r2.getNum() << "/" << r2.getDenom() << std::endl;
