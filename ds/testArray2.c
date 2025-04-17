653a05df5e3c42b65fde72c37055b781399c085b
aa <yeongjae.jeong@gmail.com>
2025-04-17 11:01:32 +0900

    40분 썰, 20분 수업

--- ds/testArray2.c
+++ ds/testArray2.c
@@ -14,6 +14,7 @@ int sumArray2(int arr[]){
 // int sumArray2(int arr[10]){ -> 숫자는 무시된다, arr은 포인터이다(인자에서 대괄호는 배열이 아니라 포인터이기 때문)
     int sum = 0;
     for (int i = 0; i < 10; ++i){
+    // for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); ++i){ // 운영체제마다 다르고 헷갈리는 상황이라 동적으로 적용할거 아니면 쓰지마라
         // sum += arr[i];
         sum += *arr++;
     }
