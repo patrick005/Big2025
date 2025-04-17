e812dceb381cb6dedb4a514fab1e326a68d5036b
aa <yeongjae.jeong@gmail.com>
2025-04-17 15:56:37 +0900

    대표님 퇴근... 최과장님 출근...

---
+++ cpp/stack2/stack.cpp
@@ -0,0 +1,72 @@
+// 
+#include "stack.h"
+#include <cassert>
+#include <cstdio>
+
+// const int STACK_SIZE = 100;
+const int Stack::STACK_SIZE = 100;
+
+// Stack::Stack(int size)
+// : pArr_(new int[size]), size_(size), tos_(0)  //요래 쓰면 this-> 대신 _만 뒤에 붙이면 됨
+// {
+//     // this->pArr = new int[size];
+//     // // assert(this->pArr != NULL);//NULl대비
+//     // this->size = size;
+//     // this->tos = 0;
+// }
+Stack::Stack(int size)
+: arr_(size), tos_(0)
+{//has-a 관계도 일종의 상속이다
+    
+}
+Stack::~Stack(){ //Array쪽의 소멸자에도 delete가 있고 거기서 처리가 될거다
+    // delete [] pArr_;
+}
+
+bool Stack::isEmpty() const{
+    return tos_ == 0;
+}
+bool Stack::isFull() const{
+    // return tos_ == size_;
+    return tos_ == arr_.size;
+}
+int Stack::size() const{
+    // return size_;
+    return arr_.size();
+}
+int Stack::avab() const{
+    // return size_ - tos_;
+    return arr_.size() - tos;
+}
+
+void Stack::push(int data){
+    assert(!this->isFull());
+
+    // pArr_[tos_] = data;
+    arr_[tos_] = data;
+    ++tos_;
+}
+
+int Stack::pop(){
+    assert(!this->isEmpty());
+
+    --tos_;
+    // return pArr_[tos_];
+    return arr_[tos_];
+}
+
+//stack3은 has-a관계를 이용하여 파일을 구축함
+//stack has a array?
+/*
+stack3은
+array파일에서 헤더랑 array.o들고오고
+stack2에서 main.cpp main.o stack.cpp stack.o stack.h 들고와야함
+거기서  stack.cpp와 stack.h 만 수정해서 컴파일함
+
+~/Big2025/cpp/stack3$ g++ -o testStack  main.o stack.o array.o
+~/Big2025/cpp/stack3$ ./testStack 
+main.cpp 추가로 컴파일하려고 시도하면 오류남
+  ** stack.h:13:9: error: ‘Array’ does not name a type **
+stack2에서 컴파일한거 들고와야함
+
+*/
---
+++ cpp/stack3/stack.cpp
@@ -0,0 +1,48 @@
+// 
+#include "stack.h"
+#include <cassert>
+#include <cstdio>
+
+// const int STACK_SIZE = 100;
+const int Stack::STACK_SIZE = 100;
+
+Stack::Stack(int size)
+: pArr_(new int[size]), size_(size), tos_(0)  //요래 쓰면 this-> 대신 _만 뒤에 붙이면 됨
+{
+    // this->pArr = new int[size];
+    // // assert(this->pArr != NULL);//NULl대비
+    // this->size = size;
+    // this->tos = 0;
+}
+
+Stack::~Stack(){
+    delete [] pArr_;
+}
+
+bool Stack::isEmpty() const{
+    return tos_ == 0;
+}
+bool Stack::isFull() const{
+    return tos_ == size_;
+}
+int Stack::size() const{
+    return size_;
+}
+int Stack::avab() const{
+    return size_ - tos_;
+}
+
+void Stack::push(int data){
+    assert(!this->isFull());
+
+    pArr_[tos_] = data;
+    ++tos_;
+}
+
+int Stack::pop(){
+    assert(!this->isEmpty());
+
+    --tos_;
+    return pArr_[tos_];
+}
+
