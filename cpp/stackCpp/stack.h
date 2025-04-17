e9d5d9a9b29e9f0c37e9c341838224d4f8d326fe
aa <yeongjae.jeong@gmail.com>
2025-04-17 15:01:33 +0900

    하래서 했지만 제네릭은 구현 못함

---
+++ cpp/stackCpp/stack.h
@@ -0,0 +1,21 @@
+// ./cpp/stackCpp/stack.h
+#ifndef STACK_H
+#define STACK_H
+
+class Stack{
+    private:
+        int *pArr;
+        int size;
+        int tos;
+    
+    public:
+        Stack(int size);
+        ~Stack();
+        void push(int data);
+        int pop();
+
+};
+
+
+
+#endif
