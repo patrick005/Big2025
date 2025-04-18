59233e8ae8231263637cfec89f446ef151b11d38
aa <yeongjae.jeong@gmail.com>
2025-04-18 11:40:58 +0900

    이거보면되요

---
+++ cpp/boundArray/main.cpp
@@ -0,0 +1,28 @@
+//이는 손자 파일임
+
+#include <iostream>
+#include "boundArray.h"
+
+int main(){
+
+    BoundArray arr1(1, 11);     //index : [0:11)
+    BoundArray arr2(10);        //index : [0:10)
+    BoundArray arr3;            //index : [0:100)
+
+    for (int i = arr1.lower(); i != arr1.upper(); ++i){
+        arr1[i] = i;
+    }
+    
+    const BoundArray arr4 = arr1;
+
+    arr4 = arr1;
+
+    arr4 == arr1;
+
+    for (int i = arr4.lower(); i != arr4.upper(); ++i){
+        std::cout << arr4[i] << std::endl;
+    }
+
+    return 0;
+
+}
