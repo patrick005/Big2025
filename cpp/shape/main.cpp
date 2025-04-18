6de207b622f2eba5b62bdd528740b5202ec0cee7
aa <yeongjae.jeong@gmail.com>
2025-04-18 15:02:34 +0900

    Shape 생성

---
+++ cpp/shape/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "shape.h"
+
+
+int main() {
+    // Shape s;
+    /*
+    cannot declare variable ‘s1’ to be of abstract type ‘Shape’
+    because the following virtual functions are pure within ‘Shape’:
+    */
+    Shape *ps; // 추상이라 위 방식은 안되지만 포인터로는 가능함
+
+
+
+
+    
+    return 0;
+}
