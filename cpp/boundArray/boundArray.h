de42da57715437919fd5a514355c80a6ade427df
aa <yeongjae.jeong@gmail.com>
2025-04-18 11:40:58 +0900

    이거보면되요

---
+++ cpp/boundArray/boundArray.h
@@ -0,0 +1,35 @@
+#ifndef BOUNDARRAY_H
+#define BOUNDARRAY_H
+
+#include "safeArray.h"
+
+class BoundArray : public SafeArray{
+private:
+
+
+public:
+    // // SafeArray(){}
+    // // SafeArray(const SafeArray& ){ /*memberwise copy*/ }
+    // // ~SafeArray(){}
+    // // SafeArray& operator=(const SafeArray& ){ /*memberwise copy*/ }
+    // // SafeArray *operator&() {return this;}
+    // // const SafeArray* operator&() const {return this};
+
+    // explicit SafeArray(int size = Array::getDefaultArraySize());
+    // SafeArray(const int *pArr , int size);
+    // SafeArray(const SafeArray& rhs);
+    // // ~SafeArray(){}
+    // SafeArray& operator=(const SafeArray& rhs);
+
+    // bool operator==(const SafeArray& rhs)const;
+
+    // virtual int& operator[](int index);
+    // virtual const int& operator[](int index) const;
+    // // int size(); // array쪽에 이미 있기에 가져오면 됨 -> Array::size() is inherited by public
+
+    void upper();
+    void lower();
+
+};
+
+#endif
