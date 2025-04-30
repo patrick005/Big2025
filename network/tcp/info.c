44bab87a524e3e4c2858aafcface905f86325742
aa <yeongjae.jeong@gmail.com>
2025-04-30 11:34:21 +0900

    Implement TCP file client and server with error handling and buffer management

--- network/tcp/info.c
+++ network/tcp/info.c
@@ -1,26 +1,35 @@
-//file_server.c
+//info.c
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/ioctl.h>
+#include <linux/sockios.h>
 
 #define BUF_SIZE 30
 
 void error_handling(char *message);
 
 int main(int argc, char *argv[]){
+    printf("INADDR_ANY: %d \n", INADDR_ANY);
+    printf("INADDR_LOOPBACK: %d \n", INADDR_LOOPBACK);
+    printf("AF_INET: %d \n", AF_INET);
+    printf("AF_INET6: %d \n", AF_INET6);
+
     int serv_sock;
     int clnt_sock;
     int str_len;
     char message[BUF_SIZE];
     int read_cnt;
     FILE *fp;
+    int snd_buf, rcv_buf, state;
 
     struct sockaddr_in serv_addr;
     struct sockaddr_in clnt_addr;
     socklen_t clnt_addr_size;
+    socklen_t len;
 
     if (argc != 2){
         printf("사용법: %s <PORT>\n", argv[0]);
@@ -31,6 +40,9 @@ int main(int argc, char *argv[]){
     if (serv_sock == -1){                                                         // socket: 소켓 생성
         error_handling("socket() error");
     }
+
+    state = getsockopt(serv_sock, SOL_SOCKET, SO_SNDBUF, (void *)&snd_buf, &len);
+    printf("서버 소켓의 snd buffer size: %d \n", snd_buf);
 
     fp = fopen("/home/aa/Big2025/network/tcp/file_server.txt", "rb");
 
@@ -54,8 +66,13 @@ int main(int argc, char *argv[]){
         error_handling("accept() error");
     }else{
         printf("connected client: %s \n", inet_ntoa(clnt_addr.sin_addr));
+        printf("connected client port: %d \n", ntohs(clnt_addr.sin_port));
     }
 
+    state = getsockopt(clnt_sock, SOL_SOCKET, SO_SNDBUF, (void *)&rcv_buf, &len);
+    printf("서버 소켓의 rcv buffer size: %d \n", rcv_buf);
+
+    int outQ;
     // 연결된 상태의 코드....
     while(1){
         read_cnt = fread((void *)message, 1, BUF_SIZE, fp);
@@ -65,6 +82,10 @@ int main(int argc, char *argv[]){
             break;
         }
         write(clnt_sock, message, BUF_SIZE);
+        ioctl(clnt_sock, SIOCOUTQ, &outQ); // 송신 큐의 크기를 확인하는 ioctl
+        printf("현재 클라이언트 sndQ의 크기: %d bytes\n", outQ); // 송신 큐의 크기를 확인하는 ioctl
+        ioctl(serv_sock, SIOCOUTQ, &outQ); // 송신 큐의 크기를 확인하는 ioctl
+        printf("현재 서버 sndQ의 크기: %d bytes\n", outQ); // 송신 큐의 크기를 확인하는 ioctl 
     }
 
     // 세련된 종료
@@ -73,7 +94,10 @@ int main(int argc, char *argv[]){
     // shutdown(clnt_sock, SHUT_RD); // recvQ흐름을 방해한다
     shutdown(serv_sock, SHUT_RD); // 서버의 recvQ는 클라이언트 패킷 전달에 영향을 미치지 않음
     read(clnt_sock, message, BUF_SIZE); // 클라이언트가 종료할 때까지 대기
+
     printf("Message from client: %s \n", message); // sendQ 종료, recvQ 살아있음
+
+
     fclose(fp);
     close(clnt_sock);
     close(serv_sock);
