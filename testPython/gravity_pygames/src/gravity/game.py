5e0eb80b59772d5733e24d69c51fbbb417631333
aa <yeongjae.jeong@gmail.com>
2025-05-15 12:27:37 +0900

    321321321

--- testPython/gravity_pygames/src/gravity/game.py
+++ testPython/gravity_pygames/src/gravity/game.py
@@ -4,7 +4,15 @@ import pygame
 class Game:
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((300,400))
+        self.screen = pygame.display.set_mode((800,600))
+        self.game_active = True
 
     def run(self):
-        pass
+        while True:
+            if self.game_active:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                        exit()
+            else:
+                break
